5b364d2534f51868f76eec5ab6960f06cbe2e9b9
Akshatha416 <107977782+Akshatha416@users.noreply.github.com>
2022-06-28 13:29:45 +0530

    Add files via upload

---
+++ AmazonApp/src/main/java/com/example/demo/BookAppEntity/BooksEntity.java
@@ -0,0 +1,61 @@
+package com.example.demo.BookAppEntity;
+
+
+
+public class BooksEntity {
+	
+	private Integer id;
+	private String author;
+	private String title;
+	private Double price;
+	
+	public BooksEntity() {
+		
+		super();
+		
+	}
+	
+	public BooksEntity(Integer id, String author, String title, Double price) {
+		super();
+		this.id = id;
+		this.author = author;
+		this.title = title;
+		this.price = price;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+	
+
+
+}
